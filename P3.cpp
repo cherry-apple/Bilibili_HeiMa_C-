2af304496dee716b491c2fe2858b0e0438a2a7e2
cherry-apple <64317377+cherry-apple@users.noreply.github.com>
2020-10-05 23:03:47 +0800

    Add files via upload

---
+++ P3.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include <unistd.h>
+using namespace std;
+
+/*
+这是多行注释
+main函数是一个程序的入口
+每个程序有且仅有一个main函数
+*/
+
+//这是单行注释
+
+int main()
+{
+    //输出hello world
+    cout<<"Hello World_hrh"<<endl;
+    pause();
+    return 0;
+}
